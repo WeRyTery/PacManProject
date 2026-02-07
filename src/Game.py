0017c94e90a32fe6dc9880d25f3bf7c95705d74d
Ghost <echanakly@gmail.com>
2026-02-08 00:05:03 +0200

    Game over screen
    - Added game over screen when user loses

--- src/Game.py
+++ src/Game.py
@@ -1,10 +1,10 @@
 import pygame as pg
-from pacman.constants import WIDTH, HEIGHT, BLACK, SECONDS_TO_FRUIT_SPAWN, FRUIT_POSITION
+from pacman.constants import *
 from pacman.board import Board
 from pacman.pacman import Pacman
 from pacman.score import Score
 from pacman.ghost_handler import GhostHandler
-from pacman.event_bus import FRUIT_SPAWN
+from pacman.event_bus import FRUIT_SPAWN, LOST_GAME
 
 FPS = 60
 pg.init() 
@@ -24,6 +24,7 @@ fruit_y = FRUIT_POSITION[1]
 
 def main():
     run = True
+    active_window = True
     clock = pg.time.Clock()
 
     while run:
@@ -33,14 +34,16 @@ def main():
                 board.spawn_fruit()
             elif event.type == pg.QUIT:
                 run = False
+                active_window = False
             pacman.handle_keys(event)
 
         pacman.update()
         
         # Если призраки съели все жизни — Game Over
         if ghost_handler.update(pacman):
-            print("Game Over!")
+            pg.event.post(pg.event.Event(LOST_GAME))
             run = False
+            break
 
         WIN.fill(BLACK)
         board.draw_board(WIN)
@@ -49,7 +52,22 @@ def main():
         score.draw(WIN, ghost_handler.lives)
         
         pg.display.update()
-            
+    
+
+    while active_window: # Game is active untill user closes window
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                active_window = False
+            elif event.type == LOST_GAME:
+                game_over(WIN)
+                pg.event.post(pg.event.Event(LOST_GAME))
     pg.quit()
+
+def game_over(window):
+    window.fill(BLACK)
+    font = pg.font.SysFont("arial", 200, bold=True)
+    text = font.render("Game over!", True, WHITE)
+    window.blit(text, (WIDTH // 15, HEIGHT // 3))
+    pg.display.update()
 
 main()
--- src/pacman/event_bus.py
+++ src/pacman/event_bus.py
@@ -1,3 +1,4 @@
 import pygame as pg
 
 FRUIT_SPAWN = pg.event.custom_type()
+LOST_GAME = pg.event.custom_type()
